03f04293279f109425c05131b1e5cbc42f24e5e4
Brad Robertson <bradleyrobertson@gmail.com>
2013-02-03 16:56:09 -0500

    first twitter-like app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterApp::Application.config.secret_token = 'bae4d0856e6e0ad61ae22c0435e957696975592a9978657793b2979bbe44ab474ca52df02a20ae9697fe5ca91157ad4b457186e2e157a055a3bee330db7ee63b'
